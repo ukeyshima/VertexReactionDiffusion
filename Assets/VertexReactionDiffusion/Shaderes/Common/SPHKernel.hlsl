5f5d7ece13625367dd6f1fc66cab2e436c7c3be0
shimadayuki <ukeyshima@gmail.com>
2023-05-31 12:35:31 +0900

    Change Kernel to CubicSpline from ViscosityLaplacian

--- Assets/VertexReactionDiffusion/Shaderes/Common/SPHKernel.hlsl
+++ Assets/VertexReactionDiffusion/Shaderes/Common/SPHKernel.hlsl
@@ -19,4 +19,15 @@ float ViscosityLaplacian(float r, float h) {
     return r > 0.0 && r < h ? 45 / (PI * pow(abs(h), 6.0)) * (h - r) : 0.0;
 }
 
+float CubicSpline(float r, float h)
+{
+    float q = abs(r) / h;
+    float a = 1 / (PI * h * h * h);
+    return a * (q > 0.0 && q < 1.0
+                    ? 1 - 3 / 2 * q * q + 3 / 4 * q * q * q
+                    : q >= 1.0 && q < 2.0
+                    ? 1 / 4 * pow(2 - q, 3)
+                    : 0.0);
+}
+
 #endif //INCLUDED_SPH_KERNEL
